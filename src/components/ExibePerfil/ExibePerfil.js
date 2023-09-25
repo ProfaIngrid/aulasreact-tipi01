6c68b87b835f671d0fd853563da16c6d667ad919
INGRID BATISTA CONCEICAO <ingrid.bconceicao@senacsp.edu.br>
2023-09-25 12:11:51 -0300

    aula03

--- src/components/ExibePerfil/ExibePerfil.js
+++ src/components/ExibePerfil/ExibePerfil.js
@@ -1,9 +1,11 @@
+import './exibePerfil.css';
+
 function ExibePerfil({nome, img, caracteristica}){
     return(
         <div>
-            <img src={img} alt={nome}/>
+            <img className="img-exibe-perfil" src={img} alt={nome}/>
             <h1>Nome: {nome}</h1>
-            <p>Caracteristicas: {caracteristica}</p>
+            <p>Habilidades: {caracteristica}</p>
         </div>
     )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,22 @@
 import ExibePerfil from '../ExibePerfil/ExibePerfil';
 import NarutoPerfil from '../../images/narutoPerfil.webp';
 import SasukePerfil from '../../images/sasukePerfil.webp';
+import Sakura from '../../images/sakuraPerfil.webp';
+import Kakashi from '../../images/kakashiPerfil.webp';
+import './main.css';
 
 function Main() {
+    let caracKakashi = "Ele é conhecido por seu domínio em diversas técnicas ninja, sendo especialmente famoso por seu uso do Sharingan (um poderoso jutsu ocular) e por sua habilidade de copiar técnicas de outros ninjas.";
+    let caracNaruto = "Ele possui uma habilidade única chamada Chakra da Kyuubi, que lhe confere grande poder. Além disso, ele é habilidoso em técnicas de clonagem e é famoso por sua técnica Rasengan";
+    let caracSasuke = "Ele é um usuário talentoso do Sharingan e, mais tarde, desenvolve o Mangekyou Sharingan. Sasuke também se torna um mestre em técnicas de manipulação de raios, conhecidas como Raiton";
+    let caracSakura = " Ela é altamente treinada em técnicas médicas e se destaca na manipulação de chakra para curar ferimentos. Além disso, ela desenvolve habilidades de combate notáveis";
+
     return(
         <main>
-            <ExibePerfil nome="Naruto" img={NarutoPerfil}/>
-            <ExibePerfil nome="Sasuke" img={SasukePerfil}/>
+            <ExibePerfil nome="Kakashi" caracteristica={caracKakashi} img={Kakashi}/>
+            <ExibePerfil nome="Naruto" caracteristica={caracNaruto} img={NarutoPerfil}/>
+            <ExibePerfil nome="Sasuke" caracteristica={caracSasuke} img={SasukePerfil}/>
+            <ExibePerfil nome="Sakura" caracteristica={caracSakura} img={Sakura}/>
         </main>
     )
 }
