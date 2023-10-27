86a9af77fe0369799f94f39747907ddb088b3997
INGRID BATISTA CONCEICAO <ingrid.bconceicao@senacsp.edu.br>
2023-10-27 12:10:54 -0300

    aula 10

--- src/components/ConsumindoApi/ConsumindoApi.js
+++ src/components/ConsumindoApi/ConsumindoApi.js
@@ -1,42 +1,53 @@
 import { useState } from "react";
 import axios from 'axios';
+import './consumoApi.css';
 
 function ConsumindoApi(){
     const [buscaCep, setBuscaCep] = useState();
-    const [data, setData] = useState([]);
-
+    const [data, setData] = useState();
+    const [cepInvalido, setCepInvalido] = useState();
     const api = `https://viacep.com.br/ws/${buscaCep}/json`;
 
     function buscaApi(e){
         e.preventDefault();
         axios.get(api)
         .then(response => {
-            console.log(response.data);
-            setData(response.data);
-        }).catch(error => {
-            console.log(error);
+            if(response.data.erro === true){
+                setData([]);
+                setCepInvalido("Cep Inválido");
+                setData("");
+            }else{
+                setData(
+                    response.data.logradouro + " " +
+                    response.data.bairro + " " +
+                    response.data.localidade+ " " +
+                    response.data.uf+ " " +
+                    response.data.cep
+                )
+                setCepInvalido("")
+            }
+        }).catch((error) => {
+            console.log(error)
+            setData("");
+            setCepInvalido("Cep Inválido")
         })
     }
     
     return(
-        <>
-        <form>
-            <label>Pesquisar usuário: </label>
+        <div className="main-cep">
+        <form className="form-cep">
+            <label className="title-cep">BUSCA CEP</label>
             <br/>
-            <input type="text" value={buscaCep} onChange={(e) => setBuscaCep(e.target.value)}/>
-            <br/>
-            <button onClick={buscaApi}>Pesquisar</button>
+            <div className="input-cep">
+                <input type="text" value={buscaCep} onChange={(e) => setBuscaCep(e.target.value)}/>
+                <button onClick={buscaApi}>IR</button>
+            </div>
         </form>
-        <p>{
-            data.length === 0 ? 'Digite um cep para buscar' 
-            :
-            `${data.logradouro} - 
-            ${data.bairro}, 
-            ${data.localidade} -
-            ${data.uf},
-            ${data.cep}`
-        }</p>
-        </>
+        <p className="resposta-cep">
+            {cepInvalido} 
+            {data}  
+        </p>
+        </div>
     )
 }
 
