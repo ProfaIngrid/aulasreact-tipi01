75c16588ca0cf0fdf9e0692ba3bdc7a6a9a23070
INGRID BATISTA CONCEICAO <ingrid.bconceicao@senacsp.edu.br>
2023-10-27 09:01:49 -0300

    aula 10

--- src/components/ConsumindoApi/ConsumindoApi.js
+++ src/components/ConsumindoApi/ConsumindoApi.js
@@ -2,16 +2,17 @@ import { useState } from "react";
 import axios from 'axios';
 
 function ConsumindoApi(){
-    const [buscaUser, setBuscaUser] = useState();
-    const api = 'https://api.postmon.com.br/v1/cep/' + buscaUser;
+    const [buscaCep, setBuscaCep] = useState();
+    const [data, setData] = useState([]);
+
+    const api = `https://viacep.com.br/ws/${buscaCep}/json`;
 
     function buscaApi(e){
         e.preventDefault();
-        console.log(api);
-
         axios.get(api)
         .then(response => {
             console.log(response.data);
+            setData(response.data);
         }).catch(error => {
             console.log(error);
         })
@@ -21,7 +22,7 @@ function ConsumindoApi(){
         <form>
             <label>Pesquisar usuário: </label>
             <br/>
-            <input type="text" value={buscaUser} onChange={(e) => setBuscaUser(e.target.value)}/>
+            <input type="text" value={buscaCep} onChange={(e) => setBuscaCep(e.target.value)}/>
             <br/>
             <button onClick={buscaApi}>Pesquisar</button>
         </form>
